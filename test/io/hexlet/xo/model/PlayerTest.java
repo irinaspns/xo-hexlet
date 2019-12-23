3e7014ceb3f778817d5ab725aa764074ba48d462
zolotar <irinaspns@gmail.com>
2019-12-23 21:06:12 +0100

    PlayerTest created

--- test/io/hexlet/xo/model/PlayerTest.java
+++ test/io/hexlet/xo/model/PlayerTest.java
@@ -8,9 +8,23 @@ public class PlayerTest {
 
     @Test
     public void getName() {
+        String inputValue = "Slava";
+        String expectedValue = inputValue;
+        final Player player = new Player(inputValue, null);
+
+        String actualValue = player.getName();
+
+        assertEquals(expectedValue, actualValue);
     }
 
     @Test
     public void getFigure() {
+        Figure inputValue = Figure.X;
+        Figure expectedValue = inputValue;
+        final Player player = new Player(null, inputValue);
+
+        Figure actualValue = player.getFigure();
+
+        assertEquals(expectedValue, actualValue);
     }
 }
