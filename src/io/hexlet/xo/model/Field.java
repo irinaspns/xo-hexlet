28d2fe0b3134ffcfe56a9a4d5f0dd4573e0f3d0c
Irina Zolotaryova <izo@klaverblad.nl>
2019-12-24 13:11:14 +0100

    Exceptions created

--- src/io/hexlet/xo/model/Field.java
+++ src/io/hexlet/xo/model/Field.java
@@ -4,17 +4,27 @@ import java.awt.*;
 
 public class Field {
     public static final int FIELD_SIZE = 3;
+    public static final int MIN_COORDINATE = 0;
+    public static final int MAX_COORDINATE = FIELD_SIZE;
     private final Figure[][] field = new Figure[FIELD_SIZE][FIELD_SIZE];
 
     public int getSize() {
         return FIELD_SIZE;
     }
 
-    public Figure getFigure(Point point) {
+    public Figure getFigure(final Point point) {
         return field[point.x][point.y];
     }
 
-    public void setFigure(Point point, Figure figure) {
+    public void setFigure(final Point point, final Figure figure) {
         field[point.x][point.y] = figure;
+    }
+
+    private boolean checkPoint(final Point point) {
+        return checkCoordinate(point.x) && checkCoordinate(point.y);
+    }
+
+    private boolean checkCoordinate(final int coordinate) {
+        return coordinate >= MIN_COORDINATE && coordinate < MAX_COORDINATE;
     }
 }
