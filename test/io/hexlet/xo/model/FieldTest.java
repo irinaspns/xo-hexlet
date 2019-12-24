12177a1d344fe281b7d214a34c94abc40fca8ea8
Irina Zolotaryova <izo@klaverblad.nl>
2019-12-24 13:30:27 +0100

    Test added

--- test/io/hexlet/xo/model/FieldTest.java
+++ test/io/hexlet/xo/model/FieldTest.java
@@ -1,5 +1,6 @@
 package io.hexlet.xo.model;
 
+import io.hexlet.xo.model.exceptions.AlreadyOccupiedException;
 import io.hexlet.xo.model.exceptions.InvalidPointException;
 import org.junit.Test;
 
@@ -18,6 +19,20 @@ public class FieldTest {
 
     @Test
     public void setFigure() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(0, 0);
+        final Figure inputFigure = Figure.O;
+
+        field.setFigure(inputPoint, inputFigure);
+        try {
+            field.setFigure(inputPoint, inputFigure);
+            fail();
+        } catch (AlreadyOccupiedException e) {
+        }
+    }
+
+    @Test
+    public void setFigureWhenAlreadyOccupied() throws Exception {
         final Field field = new Field();
         final Point inputPoint = new Point(0, 0);
         final Figure inputFigure = Figure.O;
@@ -85,4 +100,5 @@ public class FieldTest {
         } catch (final InvalidPointException e) {
         }
     }
+
 }
